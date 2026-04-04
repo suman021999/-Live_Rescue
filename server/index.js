d38c8af5af875d4e5bdb8756ac49f4ad05e1297a
Shank34 <spikbang@gmail.com>
2026-04-04 11:33:42 +0530

    add index

--- server/index.js
+++ server/index.js
@@ -171,7 +171,7 @@ io.on("connection", (socket) => {
 });
 
 // ================= START SERVER =================
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
