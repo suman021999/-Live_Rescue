d851c3f41639f7d211b1f97ab75f55912a33c0f9
Shank34 <spikbang@gmail.com>
2026-04-08 22:26:42 +0530

    fix: index

--- server/index.js
+++ server/index.js
@@ -25,10 +25,7 @@ database();
 
 // ================= CORS =================
 const corsOptions = {
-   origin: [
-      "https://live-rescue.vercel.app",
-      "http://localhost:5173"
-    ],
+   origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
 };
@@ -50,10 +47,7 @@ app.use("/api/v1/security", securitytRoutes);
 // ================= SOCKET.IO =================
 const io = new Server(server, {
   cors: {
-     origin: [
-      "https://live-rescue.vercel.app",
-      "http://localhost:5173"
-    ],
+     origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
      transports: ["websocket", "polling"],
     credentials: true,
