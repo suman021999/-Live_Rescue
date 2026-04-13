b98f63940fa4f02c1adcdea5ba6165815837831f
Shank34 <spikbang@gmail.com>
2026-04-13 17:20:12 +0530

    fix: sendmail

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -50,10 +50,8 @@ export const sendMeetingEmail = async (to, roomId) => {
     console.log("✅ Email sent:", info.response);
   } catch (err) {
     console.error("❌ SEND ERROR:", err.message);
-    
+    console.log("EMAIL_USER:", process.env.EMAIL_USER);
+    console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
     throw err; // ✅ Bubble up so caller can catch it
   }
 };
-
-// console.log("EMAIL_USER:", process.env.EMAIL_USER);
-//     console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
